d2b12f5c4c942bd3b03122835525fc749bacc523
avrilko <avrilko@avrilkodeMac-mini.local>
2020-11-25 14:41:12 +0800

    首次提交

---
+++ 算法/二分查找/binary_search.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{1, 3, 5, 9, 11, 100}
+	num := 100
+	index := binarySearch(num, arr)
+	fmt.Println(index)
+}
+
+func binarySearch(num int, arr []int) int {
+	low := 0
+	high := len(arr) - 1
+
+	for {
+		if high < low {
+			return -1
+		}
+		mid := (high + low) / 2
+		guess := arr[mid]
+		if num == guess { // 如果相同则找到了
+			return mid
+		}
+		if num > guess { // 查找的元素大于猜测的元素，则讲low设置为mid + 1
+			low = mid + 1
+		}
+		if num < guess { // 查找的元素小于猜测的元素，则将high设置为mid - 1
+			high = mid - 1
+		}
+	}
+}
